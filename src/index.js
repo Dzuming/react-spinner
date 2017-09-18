ca7daaf42823cb3d133936ac37907ef6f5aef809
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-18 09:33:20 +0200

    Remove unnecessary files to fit component to npm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, {Component} from 'react';
+import {Animation, keyFramesStyle} from './styles/Animation';
+import SpinnerStyle from './styles/Spinner';
+import Position from './styles/Position';
+import Shapes from './styles/Shapes'
+import injectStyle from './helpers/injectStyle';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Spinner extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    keyFramesStyle(props.shape).styles.map(animation => injectStyle(animation));
+  }
+
+  render() {
+    const {shape, animation, time, duration} = this.props
+    return (
+      <div>
+        <div style={{
+          ...SpinnerStyle.background
+        }}></div>
+        <div
+          style={{
+          ...Shapes[shape],
+          ...Position.center,
+          ...Animation(animation, time, duration)
+        }}></div>
+      </div>
+    );
+  }
+}
+Spinner.propTypes = {
+  animation: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  duration: PropTypes.string,
+  shape: PropTypes.string.isRequired
+};
+export default Spinner;
