840d149fb051f4787e38c0eaf0fc1c8998efcd0e
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-18 12:28:01 +0200

    Change example app.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Spinner from 'react-easy-spinner';
 class App extends Component {
   render() {
+    let settings = {
+      shape: "triangleUp",
+      animation: "run",
+      time: "2s"
+    }
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-        <Spinner shape="triangleUp" animation="pulse" time="2s"/>
+          <Spinner {...settings}/>
         </p>
       </div>
     );
