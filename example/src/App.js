3209a6546e0cc862bb3baf1fcc020b2eb189eee0
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-19 10:46:27 +0200

    Update demo

--- example/src/App.js
+++ example/src/App.js
@@ -23,36 +23,57 @@ class App extends Component {
       time: `${this.state.time}s`,
       duration: this.state.duration
     }
+    const labelWidth = {
+      width: '90px',
+      display: 'inline-block'
+    }
+    const paddingBottom = {
+      paddingBottom: '5px'
+    }
     return (
       <div className="App">
         <div style={{
           zIndex: "100",
-          position: "fixed"
+          position: "fixed",
+          margin: "10px",
+          fontSize: "16px"
         }}>
-          <select name="shape" value={this.state.shape} onChange={this.handleChange}>
-            <option value="triangleUp">triangleUp</option>
-          </select>
-          <select
-            name="animation"
-            value={this.state.animation}
-            onChange={this.handleChange}>
-            <option value="pulse">pulse</option>
-            <option value="run">run</option>
-            <option value="spin">spin</option>
-            <option value="bounceIn">bounceIn</option>
-          </select>
-          <input
-            min="1"
-            max="10"
-            type="number"
-            name="time"
-            value={this.state.time}
-            onChange={this.handleChange}/>
-          <input
-            type="text"
-            name="duration"
-            value={this.state.duration}
-            onChange={this.handleChange}/>
+          <div style={paddingBottom}>
+            <label style={labelWidth}>Shape</label>
+            <select name="shape" value={this.state.shape} onChange={this.handleChange}>
+              <option value="triangleUp">triangleUp</option>
+            </select>
+          </div>
+          <div style={paddingBottom}>
+            <label style={labelWidth}>Animation</label>
+            <select
+              name="animation"
+              value={this.state.animation}
+              onChange={this.handleChange}>
+              <option value="pulse">pulse</option>
+              <option value="run">run</option>
+              <option value="spin">spin</option>
+              <option value="bounceIn">bounceIn</option>
+            </select>
+          </div>
+          <div style={paddingBottom}>
+            <label style={labelWidth}>Time</label>
+            <input
+              min="1"
+              max="10"
+              type="number"
+              name="time"
+              value={this.state.time}
+              onChange={this.handleChange}/>
+          </div>
+          <div style={paddingBottom}>
+            <label style={labelWidth}>Duration</label>
+            <input
+              type="text"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleChange}/>
+          </div>
         </div>
         <Spinner {...settings}/>
       </div>
